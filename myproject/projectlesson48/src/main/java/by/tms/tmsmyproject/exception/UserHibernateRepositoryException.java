15ff548618d05e3ba7babefc54962c1b283994c1
Turtoises <turtoises22@yandex.by>
2022-12-23 20:01:52 +0300

    Lesson48

---
+++ myproject/projectlesson48/src/main/java/by/tms/tmsmyproject/exception/UserHibernateRepositoryException.java
@@ -0,0 +1,8 @@
+package by.tms.tmsmyproject.exception;
+
+public class UserHibernateRepositoryException extends RuntimeException{
+
+    public UserHibernateRepositoryException(String message) {
+        super(message);
+    }
+}
