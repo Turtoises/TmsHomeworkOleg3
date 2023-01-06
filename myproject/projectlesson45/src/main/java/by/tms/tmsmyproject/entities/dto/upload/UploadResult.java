4c8c4cb9eb188261af97e1365ae9d2559cda21f9
Turtoises <107067553+Turtoises@users.noreply.github.com>
2023-01-06 20:24:21 +0300

    Lesson 45
    Lesson 45

---
+++ myproject/projectlesson45/src/main/java/by/tms/tmsmyproject/entities/dto/upload/UploadResult.java
@@ -0,0 +1,15 @@
+package by.tms.tmsmyproject.entities.dto.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadResult {
+
+    private String message;
+}
